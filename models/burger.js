e0c78be4782f2a33a2165a42b63b768c3bf3fe16
Garrett Lee <rgl10d@gmail.com>
2020-12-03 16:16:18 -0500

    added burger.js model

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// IMPORT ORM
+const orm = require("../config/orm.js");
+
+// 
+const burger = {
+    all: function(cb) {
+      orm.all("cats", function(res) {
+        cb(res);
+      });
+    },
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, (res) => {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, (res) => {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, (res) => {
+        cb(res);
+      });
+    }
+  };
+  
+  // EXPORT DATABASE FUNCTION FOR CONTROLLER
+  module.exports = burger;
